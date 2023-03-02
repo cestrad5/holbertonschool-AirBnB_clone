757501127455524fc01ae751b56bd124da7f4ee0
CAMILO <cestrad5@gmail.com>
2023-03-02 12:36:26 -0500

    Creating empty files and diectories to be ready and structure the project

--- console.py
+++ console.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-"""This console will allow you to create,delete, show, update or destroy variables at the database for the AirBnB project"""
+"""This console will allow you to create,
+delete, show, update or destroy variables
+at the database for the AirBnB project"""
 
